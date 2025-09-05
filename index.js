f3cca60d43b5d704a64ae68047d5bd769b21b32a
Grace-Racasa <racasa.grace1@gmail.com>
2025-09-05 10:32:46 +0800

    commited javascript

---
+++ index.js
@@ -0,0 +1,58 @@
+/*console.log("hello world");
+
+let student = "Grace Racasa";
+let grade = 1.00;
+let isPassed = true;
+let address;
+let partner = null;
+
+
+console.log("Student Name: " +student + "\n" + "Student Grade: "+ grade + "\n" + "Student Passed: " + isPassed+ "\n" + "Address: "+ address + "\n"+ "Partner: " + partner+ "\n" );
+*/
+
+/*
+let num1 = 10;
+let num2 = 5;
+
+let sum = num1+num2;
+let diff = num1-num2;
+let prod = num1*num2;
+let quo = num1/num2;
+let mod = num1%num2;
+
+
+console.log("Sum: " + sum + "\n" + "Difference: " + diff + "\n" + "Product: " + prod + "\n" + "Quotient: " + quo + "\n" + "Modulus: " + mod);
+*/
+// let grade = 70;
+
+// if(grade > 75){
+//     console.log("Students Passed!");
+// } else {
+//     console.log("Students Failed!");
+// }
+
+
+// loop
+
+// for (let i = 0; i <= 5; i++){
+//     console.log("Count: " + i);
+// }
+
+// while loop
+
+// let i = 0;
+// while(i <= 5){
+//     console.log("Count: " + i);
+//     i++;
+// }
+
+// function
+
+function greet(name){
+    console.log("Hello " + name + "!");
+}
+
+greet("Grace");
+greet("Racasa");
+greet("John");
+greet("Doe");
