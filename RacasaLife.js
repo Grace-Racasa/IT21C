c3fb15b99e07b9286a4b147a0a548026eca160e2
Grace-Racasa <racasa.grace1@gmail.com>
2025-09-09 09:59:09 +0800

    Added Student Variable

---
+++ RacasaLife.js
@@ -0,0 +1,7 @@
+// Declare Variables
+let name = "Grace Racasa";
+let course = "Bachelor of Science in Information Technology";
+let yearLevel = "secomd year";
+
+console.log("Name: " + name + "\n" + "Course: " + course + "\n" + "Year Level: " + yearLevel);
+
