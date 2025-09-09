3a38afc79ce40317129cd68653e6c16724a90245
Grace-Racasa <racasa.grace1@gmail.com>
2025-09-09 10:17:42 +0800

    Added condition for study hours

--- RacasaLife.js
+++ RacasaLife.js
@@ -11,4 +11,12 @@ let hoursOfStudy = 2;
 
 console.log("Total Hours of Sleep: " + hoursOfSleep + "\n"+ "Total Hours of Sleep: " + hoursOfStudy +"\n");
 
+// conditions 
+
+if (hoursOfStudy >= 5){
+    console.log("Great! You Studied enough today.");
+} else {
+    console.log("You need study more.");
+}
+
 
