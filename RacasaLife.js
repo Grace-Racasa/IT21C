0937cd3060e17e40579a7a32a128ee5a296e0bda
Grace-Racasa <racasa.grace1@gmail.com>
2025-09-09 14:50:18 +0800

    added operators for daily routine

--- RacasaLife.js
+++ RacasaLife.js
@@ -4,3 +4,9 @@ let course = "Bachelor of Science in Information Technology";
 let yearLevel = "secomd year";
 
  console.log("Name: " + name + "\n" + "Course: " + course + "\n" + "Year Level: " + yearLevel);
+
+ // Operators
+let hoursOfSleep = 8;
+let hoursOfStudy = 2;
+
+console.log("Total Hours of Sleep: " + hoursOfSleep + "\n"+ "Total Hours of Sleep: " + hoursOfStudy +"\n");
